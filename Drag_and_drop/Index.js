318027b1c0cca2f45ce60b695ca5c824b8f06867
Williamjr23 <wglopezcastillo@gmail.com>
2021-11-30 17:33:13 -0600

    drag and drop customized

--- Drag_and_drop/Index.js
+++ Drag_and_drop/Index.js
@@ -6,3 +6,40 @@ const input =dropArea.querySelector('#input-file');
 button.addEventListener('click', e => {
     input.click()
 });
+
+input.addEventListener("change", (e) => {
+    files = this.files;
+    dropArea.classList.add("active");
+    showFiles(files);
+    dropArea.classList.remove("active");
+});
+
+dropArea.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropArea.classList.add("active");
+    dragText.textContent = "Suelta para subir los archivos";
+});
+
+dropArea.addEventListener("dragleave", (e) => {
+    dropArea.classList.remove("active");
+    dragText.textContent = "Arrastra y suelta imágenes";
+});
+
+dropArea.addEventListener("drop", (e) => {
+    dropArea.classList.remove("active");
+    dragText.textContent = "Arrastra y suelta imágenes";
+});
+
+function showFiles(files) {
+    if(files.length === undefined){
+        proccessFile(files);
+    }else{
+        for(const file of files){
+            proccessFile(file);
+        }
+    }
+}
+
+function proccessFile(file){
+
+}
